9ca0df93ded44677d95d529e7f016284591dbccf
Evaldo <evaldocz18@gmail.com>
2019-09-28 17:44:13 -0300

    Tela de layout NPS aceitavel mas sem backend

--- app/src/main/java/com/evaldo/terminalperquisacliente/activity/MainActivity.java
+++ app/src/main/java/com/evaldo/terminalperquisacliente/activity/MainActivity.java
@@ -5,7 +5,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.evaldo.terminalperquisacliente.R;
+import com.evaldo.terminalperquisacliente.telasPerguntas.PerguntaPrimeiraEmotion3DivulgacaoActivity;
 import com.evaldo.terminalperquisacliente.telasPerguntas.PerguntaPrimeiraNPSActivity;
+
+import java.security.Principal;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
     private void chamarTela() {
-        Intent intent = new Intent(this, PerguntaPrimeiraNPSActivity.class);
+        Intent intent = new Intent(this, PrincipalActivity.class);
         onStop();
         startActivity(intent);
     }
