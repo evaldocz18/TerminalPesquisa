0b1776b76c0a5fb84560a2e160374f47e61a7f7e
Evaldo <evaldocz18@gmail.com>
2019-11-20 12:33:19 -0300

    Alterando a estrutura do banco dos terminais de pesquisa para ter mais variaveis (não esta pronto)

--- app/src/main/java/com/evaldo/terminalperquisacliente/classes/TerminalPesquisa.java
+++ app/src/main/java/com/evaldo/terminalperquisacliente/classes/TerminalPesquisa.java
@@ -1,19 +1,23 @@
 package com.evaldo.terminalperquisacliente.classes;
 
 import java.io.Serializable;
+import java.util.Arrays;
 
 public class TerminalPesquisa implements Serializable {
-    String nomeDispositivo,idDispositivo, status,key, dataAtivacao, questionarioAtual ;
+    String nomeDispositivo,idDispositivo, nomeFuncionario, emailFuncionario, status,key, dataAtivacao, questionarioAtual, imagemDivulgacaoEPergunta[], ultimaVezOnline[], imagemDivulgacao[] ;
 
-    public TerminalPesquisa(String nomeDispositivo, String idDispositivo, String status, String key, String dataAtivacao, String questionarioAtual) {
+    public TerminalPesquisa(String nomeDispositivo, String idDispositivo, String nomeFuncionario, String emailFuncionario, String status, String key, String dataAtivacao, String questionarioAtual, String[] imagemDivulgacaoEPergunta, String[] ultimaVezOnline, String[] imagemDivulgacao) {
         this.nomeDispositivo = nomeDispositivo;
         this.idDispositivo = idDispositivo;
+        this.nomeFuncionario = nomeFuncionario;
+        this.emailFuncionario = emailFuncionario;
         this.status = status;
         this.key = key;
         this.dataAtivacao = dataAtivacao;
         this.questionarioAtual = questionarioAtual;
-
-
+        this.imagemDivulgacaoEPergunta = imagemDivulgacaoEPergunta;
+        this.ultimaVezOnline = ultimaVezOnline;
+        this.imagemDivulgacao = imagemDivulgacao;
     }
 
     public TerminalPesquisa(){}
@@ -66,15 +70,60 @@ public class TerminalPesquisa implements Serializable {
         this.questionarioAtual = questionarioAtual;
     }
 
+    public String getNomeFuncionario() {
+        return nomeFuncionario;
+    }
+
+    public void setNomeFuncionario(String nomeFuncionario) {
+        this.nomeFuncionario = nomeFuncionario;
+    }
+
+    public String getEmailFuncionario() {
+        return emailFuncionario;
+    }
+
+    public void setEmailFuncionario(String emailFuncionario) {
+        this.emailFuncionario = emailFuncionario;
+    }
+
+    public String[] getImagemDivulgacaoEPergunta() {
+        return imagemDivulgacaoEPergunta;
+    }
+
+    public void setImagemDivulgacaoEPergunta(String[] imagemDivulgacaoEPergunta) {
+        this.imagemDivulgacaoEPergunta = imagemDivulgacaoEPergunta;
+    }
+
+    public String[] getUltimaVezOnline() {
+        return ultimaVezOnline;
+    }
+
+    public void setUltimaVezOnline(String[] ultimaVezOnline) {
+        this.ultimaVezOnline = ultimaVezOnline;
+    }
+
+    public String[] getImagemDivulgacao() {
+        return imagemDivulgacao;
+    }
+
+    public void setImagemDivulgacao(String[] imagemDivulgacao) {
+        this.imagemDivulgacao = imagemDivulgacao;
+    }
+
     @Override
     public String toString() {
         return "TerminalPesquisa{" +
                 "nomeDispositivo='" + nomeDispositivo + '\'' +
                 ", idDispositivo='" + idDispositivo + '\'' +
+                ", nomeFuncionario='" + nomeFuncionario + '\'' +
+                ", emailFuncionario='" + emailFuncionario + '\'' +
                 ", status='" + status + '\'' +
                 ", key='" + key + '\'' +
                 ", dataAtivacao='" + dataAtivacao + '\'' +
                 ", questionarioAtual='" + questionarioAtual + '\'' +
+                ", imagemDivulgacaoEPergunta=" + Arrays.toString(imagemDivulgacaoEPergunta) +
+                ", ultimaVezOnline=" + Arrays.toString(ultimaVezOnline) +
+                ", imagemDivulgacao=" + Arrays.toString(imagemDivulgacao) +
                 '}';
     }
 }
